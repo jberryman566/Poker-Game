31cfef5b5f90269c6902e3939d0e17d0a3f2aca2
Josh W Berryman <C0431394@intra.camosun.bc.ca>
2019-05-30 16:40:08 -0700

    Code clean up, commenting and some refactoring

--- PokerServer.java
+++ PokerServer.java
@@ -45,8 +45,117 @@ public class PokerServer{
 		}
 		//Deal the deck
 		DealCards();
+		playGame();
+	}
+	
+	//Plays a round of bets
+	private void PlayRound(Player littleB, Player bigB) {
+		//Retrieve little blind
 		
-		//Start Game
+		//Retrieve Big blind
+		
+		//While there is next player, Ask next player RAISE, CALL, FOLD? 
+	}
+	
+	//Preflop round. Betting is done before the flop
+	private void playPreFlop(Player littleB, Player bigB) {
+		
+	}
+	//Flop is special first round. 3 community cards are flipped.
+	private void playFlop(Player littleB, Player bigB) {
+
+	}
+	
+	//Method for dealing players hands. 2 cards to each player.
+	//Will store the players hands locally in server in the player objects.
+	private void DealCards() {
+		//Deal the players their hands and 3 cards for house
+	}
+	
+	//Player raises a bet
+	private void playerRaise(Player player) {
+		
+	}
+	
+	//Player folds their hand and leaves the round.
+	private void playerFold(Player player) {
+		
+	}
+	
+	//Player matches the bet
+	private void playerCall(Player player) {
+		
+	}
+	
+	//Method for waiting and accepting a new player connection. Returns Player.
+	private Player fetchPlayer() throws IOException {
+		
+		try {
+			System.out.println("Waiting for player to connect...");
+			// serverSocket = new ServerSocket(serverPORT);
+			clientSocket = serverSocket.accept();
+			System.out.println("Creating new player...");
+			player = createPlayer(clientSocket);
+			
+			//Get Player Name
+			System.out.println("Retrieving player data...");
+			player.Name = sendPlayerMessage("What is your Name?", player);
+			
+			if(debugMode) {System.out.println("Added new Player: " + player.Name);}
+			
+		} catch(IOException e) {
+			System.out.println("Failed to retrieve new player...");
+		}
+		return player;
+	}
+	
+	//Method for sending generic string methods to players and returns a response.
+	private String sendPlayerMessage(String message, Player player) throws IOException{
+		String response;
+		try{
+			player.pout.println(message);
+			response = player.bin.readLine();
+			
+		} catch(IOException e) {
+			response = "Failed to connect to client...";
+		}
+		
+		return response;
+	}
+	
+	//Method for creating new player objects for client connections. Returns Player object.
+	private Player createPlayer(Socket client) throws IOException {
+		
+		//Create Player object
+		player = new Player();
+		player.clientSocket = client;
+		
+		try {
+			player.pout = new PrintWriter(client.getOutputStream(), true);
+			player.bin = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		} catch(IOException e) {
+			System.out.println("Failed to connect to client...");
+		}
+		return player;
+	}
+	
+	//Method for safely stopping the server
+	public void stop() throws IOException {
+        
+		try {
+			bin.close();
+			pout.close();
+			clientSocket.close();
+			serverSocket.close();
+		} catch(IOException e) {
+			System.out.println("Failed to close Server correctly...");
+		}
+    }
+	
+	//Starts the game of poker.
+	public void playGame() {
+		//TODO
+		
 		while (readyToPlay) {
 			//Begin Game Conversation
 			
@@ -70,108 +179,6 @@ public class PokerServer{
 			//Puts little blind in back of queue
 			BlindQueue.add(littleBlind);
 		}
-	}
-	
-	//Plays a round of bets
-	private void PlayRound(Player littleB, Player bigB) {
-		//Retrieve little blind
-		
-		//Retrieve Big blind\
-		
-		//While there is next player, Ask next player RAISE, CALL, FOLD? 
-	}
-	
-	//Preflop round. Betting is done before the flop
-	private void playPreFlop(Player littleB, Player bigB) {
-		
-	}
-	//Flop is first round. 3 community cards are flipped.
-	private void playFlop(Player littleB, Player bigB) {
-
-	}
-	
-	private void DealCards() {
-		//Deal the players their hands and 3 cards for house
-	}
-	
-	//Player raises a bet
-	private void playerRaise() {
-		
-	}
-	
-	//Player folds their hand and leaves the round.
-	private void playerFold() {
-		
-	}
-	
-	//player matches the bet
-	private void playerCall() {
-		
-	}
-	
-	private Player fetchPlayer() throws IOException {
-		
-		try {
-			System.out.println("Waiting for player to connect...");
-			// serverSocket = new ServerSocket(serverPORT);
-			clientSocket = serverSocket.accept();
-			System.out.println("Creating new player...");
-			player = createPlayer(clientSocket);
-			
-			//Get Player Name
-			System.out.println("Retrieving player data...");
-			player.Name = sendPlayerMessage("What is your Name?", player);
-			
-			if(debugMode) {System.out.println("Added new Player: " + player.Name);}
-			
-		} catch(IOException e) {
-			System.out.println("Failed to retrieve new player...");
-		}
-		return player;
-	}
-	
-	private String sendPlayerMessage(String message, Player player) throws IOException{
-		String response;
-		try{
-			player.pout.println(message);
-			response = player.bin.readLine();
-			
-		} catch(IOException e) {
-			response = "Failed to connect to client...";
-		}
-		
-		return response;
-	}
-	
-	private Player createPlayer(Socket client) throws IOException {
-		
-		//Create Player object
-		player = new Player();
-		player.clientSocket = client;
-		
-		try {
-			player.pout = new PrintWriter(client.getOutputStream(), true);
-			player.bin = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		} catch(IOException e) {
-			System.out.println("Failed to connect to client...");
-		}
-		return player;
-	}
-	
-	public void stop() throws IOException {
-        
-		try {
-			bin.close();
-			pout.close();
-			clientSocket.close();
-			serverSocket.close();
-		} catch(IOException e) {
-			System.out.println("Failed to close Server correctly...");
-		}
-    }
-	
-	public void playGame() {
-		//TODO
 	}
 	
 	public static void main(String[] args) throws IOException{
