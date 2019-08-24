71345ac45203634045d431bbbc8f5ada6fab1b00
jakob valen <jakobvalen@w134-87-182-222.wireless.uvic.ca>
2019-08-24 11:56:11 -0700

    Adding NoCardsException.java

---
+++ NoCardsException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carddeck;
+
+/**
+ *
+ * @author jakobvalen
+ */
+public class NoCardsException extends Exception { 
+    
+    public NoCardsException(String errorMessage){ 
+        super(errorMessage);
+    }
+    
+}
